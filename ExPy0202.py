ae4bc01d5dfc5c6fea88b42acaedc0f6a2516cfd
zoro6908 <zoro6908@naver.com>
2020-09-25 09:29:39 +0900

    Initial commit

---
+++ ExPy0202.py
@@ -0,0 +1,16 @@
+# 2. 갌 대입하기
+a = 255 + 488
+print(a, type(a))
+
+a = "안녕 파이썬"
+print(a, type(a))
+
+a = "대한민국"
+a = "대한민국" + str(3700)
+print(a)
+
+a = input("숫자를 입력하세요..")
+b = input("더할 숫자를 입력하세요...")
+
+print(a + b)
+print(int(a) + int(b))
